bf5ab7902a7e52a44ed336df4e789fcefbf8333c
LautaroRomano <Lautarooyt837@gmail.com>
2024-12-06 23:16:24 -0300

    quitamos expo-font

--- gym-app/app/_layout.tsx
+++ gym-app/app/_layout.tsx
@@ -1,5 +1,4 @@
 // layout.tsx
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
@@ -8,19 +7,10 @@ import "react-native-reanimated";
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
+    SplashScreen.hideAsync();
+  }, []);
 
   return (
     <Stack>
