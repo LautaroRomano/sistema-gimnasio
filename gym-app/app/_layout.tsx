b9bff908f8f68c7923db441c7defa5bb5a70ce6e
LautaroRomano <Lautarooyt837@gmail.com>
2024-12-06 23:24:13 -0300

    Revert "quitamos expo-font"
    This reverts commit bf5ab7902a7e52a44ed336df4e789fcefbf8333c.

--- gym-app/app/_layout.tsx
+++ gym-app/app/_layout.tsx
@@ -1,4 +1,5 @@
 // layout.tsx
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
@@ -7,10 +8,19 @@ import "react-native-reanimated";
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
 
   useEffect(() => {
-    SplashScreen.hideAsync();
-  }, []);
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
 
   return (
     <Stack>
